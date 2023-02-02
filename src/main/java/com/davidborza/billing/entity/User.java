dae3156c011d0a944f4889ca80d9d4ea29de90fa
David Borza <dvidborza@gmail.com>
2023-02-02 17:38:40 +0100

    Novin test project backend

--- src/main/java/com/davidborza/billing/entity/User.java
+++ src/main/java/com/davidborza/billing/entity/User.java
@@ -1,15 +1,6 @@
 package com.davidborza.billing.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDateTime;
--- src/main/java/com/davidborza/billing/service/impl/DatabaseServiceImpl.java
+++ src/main/java/com/davidborza/billing/service/impl/DatabaseServiceImpl.java
@@ -1,13 +1,20 @@
 package com.davidborza.billing.service.impl;
 
+import com.davidborza.billing.entity.Role;
+import com.davidborza.billing.entity.User;
+import com.davidborza.billing.enums.RoleTypeEnum;
 import com.davidborza.billing.repository.RoleRepository;
 import com.davidborza.billing.repository.UserRepository;
 import com.davidborza.billing.service.DatabaseService;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import java.util.HashSet;
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created on 2023. 02. 02.
@@ -27,25 +34,25 @@ public class DatabaseServiceImpl implements DatabaseService {
      */
     @Override
     @PostConstruct
+    @Transactional(rollbackFor = Exception.class)
     @SuppressWarnings("checkstyle:IllegalCatch")
     public void init() {
         final EntityManager entityManager = this.entityManagerFactory.createEntityManager();
 
-        try {
-            entityManager.getTransaction().begin();
-            final boolean rolesWasEmpty = this.initRoles(entityManager);
-            final boolean usersWasEmpty = this.initUsers(entityManager);
-            this.initUserRoles(entityManager, rolesWasEmpty, usersWasEmpty);
-            entityManager.getTransaction().commit();
-        } catch (final Exception e) {
-            entityManager.getTransaction().rollback();
+        if (roleRepository.count() == 0 && userRepository.count() == 0) {
+            try {
+                entityManager.getTransaction().begin();
+                this.initRoles(entityManager);
+                entityManager.getTransaction().commit();
+                this.initUsers();
+            } catch (final Exception e) {
+                entityManager.getTransaction().rollback();
+            }
         }
     }
 
-    private boolean initRoles(final EntityManager entityManager) {
-        final boolean isEmpty = this.roleRepository.count() == 0;
-
-        if (isEmpty) {
+    private void initRoles(final EntityManager entityManager) {
+        if (this.roleRepository.count() == 0) {
             final String query = """
                     INSERT INTO roles(id, name, description) VALUES
                         (1, 'ROLE_ADMINISTRATOR', 'Role Administrator.'),
@@ -54,36 +61,31 @@ public class DatabaseServiceImpl implements DatabaseService {
                     """;
             entityManager.createNativeQuery(query).executeUpdate();
         }
-
-        return isEmpty;
     }
 
-    private boolean initUsers(final EntityManager entityManager) {
-        final boolean isEmpty = this.userRepository.count() == 0;
+    private void initUsers() {
+        if (userRepository.count() == 0) {
+            final User user1 = new User();
+            user1.setUsername("admin");
+            user1.setName("Admin User");
+            user1.setPassword("$2a$10$NIxICnmB1lhzuE6KHps0UeSwSCeSA1WaQHZwSGSO78/A9Z9Qr3Yc.");
+            user1.setRoles(new HashSet<Role>(List.of(roleRepository.findByName(RoleTypeEnum.ROLE_ADMINISTRATOR).get())));
 
-        if (isEmpty) {
-            final String query = """
-                    INSERT INTO users(id, name, username, password, last_log_in) VALUES
-                        (1, 'user1', 'user','$2a$10$NIxICnmB1lhzuE6KHps0UeSwSCeSA1WaQHZwSGSO78/A9Z9Qr3Yc.', null),
-                        (2, 'user1', 'accountant','$2a$10$NIxICnmB1lhzuE6KHps0UeSwSCeSA1WaQHZwSGSO78/A9Z9Qr3Yc.', null),
-                        (3, 'user1', 'admin','$2a$10$NIxICnmB1lhzuE6KHps0UeSwSCeSA1WaQHZwSGSO78/A9Z9Qr3Yc.', null);
-                    """;
-            entityManager.createNativeQuery(query).executeUpdate();
-        }
+            final User user2 = new User();
+            user2.setUsername("accountant");
+            user2.setName("Accountant User");
+            user2.setPassword("$2a$10$NIxICnmB1lhzuE6KHps0UeSwSCeSA1WaQHZwSGSO78/A9Z9Qr3Yc.");
+            user2.setRoles(new HashSet<Role>(List.of(roleRepository.findByName(RoleTypeEnum.ROLE_ACCOUNTANT).get())));
 
-        return isEmpty;
-    }
+            final User user3 = new User();
+            user3.setUsername("user");
+            user3.setName("User User");
+            user3.setPassword("$2a$10$NIxICnmB1lhzuE6KHps0UeSwSCeSA1WaQHZwSGSO78/A9Z9Qr3Yc.");
+            user3.setRoles(new HashSet<Role>(List.of(roleRepository.findByName(RoleTypeEnum.ROLE_USER).get())));
 
-    private void initUserRoles(final EntityManager entityManager, final boolean rolesWasEmpty, final boolean usersWasEmpty) {
-        if (rolesWasEmpty && usersWasEmpty) {
-            final String query = """
-                    INSERT INTO user_roles(user_id, role_id) VALUES
-                        (1, 3),
-                        (2, 2),
-                        (2, 3),
-                        (3, 1);
-                    """;
-            entityManager.createNativeQuery(query).executeUpdate();
+            userRepository.save(user1);
+            userRepository.save(user2);
+            userRepository.save(user3);
         }
     }
 }
